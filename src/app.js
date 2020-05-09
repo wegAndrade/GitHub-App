5e690730d566b5f56d288b5fb5fee3da20bab2ae
Wellington Andrade <weg.andrade@gmail.com>
2020-05-09 19:49:13 -0300

    Utilizando spread operator

--- src/app.js
+++ src/app.js
@@ -68,10 +68,7 @@ class App extends Component {
   render () {
     return (
       <AppContent
-        userInfo={this.state.userInfo}
-        repos={this.state.repos}
-        starred={this.state.starred}
-        isFetching={this.state.isFetching}
+        {...this.state}
         handleSearch={(e) => this.handleSearch(e)}
         getRepos={this.getRepos('repos')}
         getStarred={this.getRepos('starred')}
